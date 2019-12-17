eae65bbd4c59299ce37cdae06c8df4e85fc62754
nekomamoushi <germione.grangere@gmail.com>
2019-12-17 17:31:29 +0100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", redirectTo: "profile", pathMatch: "full" },
+  {
+    path: "profile",
+    loadChildren: () =>
+      import("./pages/profile/profile.module").then(m => m.ProfilePageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
