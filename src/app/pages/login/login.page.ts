61fb05c9c9a38823828655a8ed7b36e653670350
nekomamoushi <germione.grangere@gmail.com>
2019-12-18 09:46:12 +0100

    feat: create login page

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        ])
+      ),
+      password: new FormControl(
+        "",
+        Validators.compose([Validators.required, Validators.minLength(8)])
+      )
+    });
+  }
+}
