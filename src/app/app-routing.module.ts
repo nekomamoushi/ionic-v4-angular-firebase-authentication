1790c9dc62c916f7e804455b1bd6bd7e8a37f140
nekomamoushi <germione.grangere@gmail.com>
2019-12-18 11:26:16 +0100

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,26 @@ import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: "", redirectTo: "profile", pathMatch: "full" },
+  { path: "", redirectTo: "login", pathMatch: "full" },
   {
     path: "profile",
     loadChildren: () =>
       import("./pages/profile/profile.module").then(m => m.ProfilePageModule)
   },
   {
-    path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    path: "register",
+    loadChildren: () =>
+      import("./pages/register/register.module").then(m => m.RegisterPageModule)
   },
   {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    path: "login",
+    loadChildren: () =>
+      import("./pages/login/login.module").then(m => m.LoginPageModule)
   },
   {
-    path: 'forgot',
-    loadChildren: () => import('./pages/forgot/forgot.module').then( m => m.ForgotPageModule)
+    path: "forgot",
+    loadChildren: () =>
+      import("./pages/forgot/forgot.module").then(m => m.ForgotPageModule)
   }
 ];
 
