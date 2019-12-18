d6ed1bc3dbc3905ec08d69dcbe9a60fafdb8d94f
nekomamoushi <germione.grangere@gmail.com>
2019-12-18 12:13:16 +0100

    feat: add guard to profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
   {
     path: "profile",
     loadChildren: () =>
-      import("./pages/profile/profile.module").then(m => m.ProfilePageModule)
+      import("./pages/profile/profile.module").then(m => m.ProfilePageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: "register",
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  UrlSegment,
+  Router
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "../services/auth.service";
+import { Route } from "@angular/compiler/src/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.isAuthenticated) {
+      this.router.navigateByUrl("/login");
+    }
+    return this.authService.isAuthenticated;
+  }
+}
